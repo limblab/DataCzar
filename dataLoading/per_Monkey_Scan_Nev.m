686ddd095b2c277e148ddf00b5d06973ce4b88f4
Kevin Bodkin <kevinbodkin2017@u.northwestern.edu>
2018-03-29 17:24:41 -0500

    modified:   dataLoading/per_Monkey_Scan_Nev.m
    	Added a bunch to the find_nev_files section of the scraper, still
    	having issues with consolodating sorted and non-sorted groups of
    	nevs.

--- dataLoading/per_Monkey_Scan_Nev.m
+++ dataLoading/per_Monkey_Scan_Nev.m
@@ -21,6 +21,8 @@ function per_Monkey_Scan_Nev(monkeyName,directoryLocation,xmlFileName)
 
 %% create the xml DOM object
 docNode = com.mathworks.xml.XMLUtils.createDocument('perMonkeyMetaData');
+updateDate = docNode.createElement('UpdateInformation');
+docNode.getDocumentElement.appendChild(updateDate);
 expList = docNode.createElement(sprintf('%sExperimentList',monkeyName)); % create the node for the experiment list
 docNode.getDocumentElement.appendChild(expList); % add the node to the document
 
@@ -31,16 +33,17 @@ docNode.getDocumentElement.appendChild(expList); % add the node to the document
 
 % stores the date, which file types are available and where, and whether
 % there is a sorted file available.
+% nevList = struct; % this is because I don't want to make it 1x1 to start. Have to figure that out.
 nevList = struct(...
     'BaseName',             '',...
     'Date',                 [],...
-    'FileTypes',            struct('Type','','Location',''),...
+    'FileTypes',            struct('Type',[],'FileLocn',[]),... % below, but if I do it now it makes this a 1x1 empty struct
     'Sorted',               false);
 
 nevList = find_Nev_Files(nevList,directoryLocation);
 
 
-
+keyboard
 
 %% load nevList structure into the xml DOM node
 
@@ -57,17 +60,96 @@ end
 function nevList = find_Nev_Files(nevList,directoryLocation)
 
 % what's in the directory?
-D = dir(directoryLocation);
-D = D(3:end); % get rid of . and ..
+subDirs = dir(directoryLocation); % find the subdirectories
+subDirs = subDirs(3:end); % get rid of . and ..
+subDirs = subDirs([subDirs.isdir]);
 
-for ii = 1:length
-    fileSplit = strsplit(D(ii).name,'.');
-    fileExt = fileSplit{end};
-    if strcmp(fileExt,'nev')
-        if any(strcmp(strsplit([fileSplit{1:end-1}],'_'),{'sorted','001'})
+localNevs = dir([directoryLocation,filesep,'*.nev']); % get the local nevs
+
+
+
+for ii = 1:length(localNevs) % for each nev
+    baseName = strsplit(localNevs(ii).name,'.nev'); % get rid of the file extension
+    baseName = baseName{1}; % switch it back to a string
+    baseNameCopy = baseName; % for looking for cds etc for sorted. There's probably a better way to do this...
+    [baseName,sortFlag] = regexp(baseName,'_(s{1}(orted)?|[0-9]{2}(?![0-9]))','split','match'); % looking for sorted stuff, get rid of that for the baseName
+    baseName = baseName{1}; % bring it back together
+    baseExists = strcmp({nevList.BaseName},baseName); % find any matching filenames - this should only be one at a time, but we'll see if things break
+    
+    if any(baseExists)
+        nevList(baseExists).FileTypes(end+1).Location = [directoryLocation,filesep,localNevs(ii).name]; % file loc'n
+        nevList(baseExists).FileTypes(end).Type = '.nev'; % we already know this - we're looking through nevs :)
         
-        nevList.BaseName = [fileSplit{1:end-1}];
-        nevList.
+        if ~isempty(sortFlag) % look for sorted tags - a little restrictive, but I'm not sure what else to do at the moment
+            nevList(baseExists).Sorted = true;
+        end
+    else
+        nevList(end+1).BaseName = baseName; % tack on a new entry
+        nevList(end).Date = localNevs(ii).date; % the storage date - that'll work for now, might change that later
+        nevList(end).FileTypes.Type = '.nev'; % because it's a list of nevs
+        nevList(end).FileTypes.Location = [directoryLocation,filesep,localNevs(ii).name]; % and the location
+        
+        if ~isempty(sortFlag)
+            nevList(end).Sorted = true; % someone sorted it, or so I think...
+        end
+    end
+        matchingFiles = dir([directoryLocation,filesep,baseNameCopy,'*']); % find a matching list of different file types, making sure not r
+        for jj = 1:length(matchingFiles)
+            fileSplit = strsplit(matchingFiles(jj).name,'.'); % get filetypes, again
+            addFile = ~(any(strcmp(fileSplit{end},{'nev','fig','png','jpg','bmp'}))||...
+                        matchingFiles(jj).isdir);
+            if addFile % other than nev and directories
+                if strcmp(fileSplit{end},'mat') % treat mat files a little differently
+                    if ~isempty(regexp([fileSplit{end-1}],'(BDF|bdf)', 'once')) % old BDFs
+                        nevList(end).FileTypes(end+1) = struct('Type','BDF',...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                    elseif ~isempty(regexp([fileSplit{end-1}],'(CDS|cds)', 'once')) % CDSs
+                        nevList(end).FileTypes(end+1) = struct('Type','CDS',...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                    elseif ~isempty(regexp([fileSplit{end-1}],'(D|d)(ecoder|ECODER)', 'once'))
+                        nevList(end).FileTypes(end+1) = struct('Type','decoder',...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                    elseif ~isempty(regexp([fileSplit{end-1}],'params', 'once'))
+                        nevList(end).FileTypes(end+1) = struct('Type','FES Params',...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                    else
+                        nevList(end).FileTypes(end+1) = struct('Type','mat',...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                    end
+                
+                elseif strcmp(fileSplit{end},'txt') % txt for FES stuff
+                    if ~isempty(regexp([fileSplit{end-1}],'emgpreds','once'))
+                        nevList(end).FileTypes(end+1) = struct('Type','FES Predictions',...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                    elseif ~isempty(regexp([fileSplit{end-1}],'params','once'))
+                        nevList(end).FileTypes(end+1) = struct('Type','FES Params',...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                    elseif ~isempty(regexp([fileSplit{end-1}],'spikes','once'))
+                        nevList(end).FileTypes(end+1) = struct('Type','FES Spikes',...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                    elseif ~isempty(regexp([fileSplit{end-1}],'stim_out','once'))
+                        nevList(end).FileTypes(end+1) = struct('Type','FES Stim Commands',...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                    elseif ~isempty(regexp([fileSplit{end-1}],'words','once'))
+                        nevList(end).FileTypes(end+1) = struct('Type','FES Words',...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                    end
+                    
+                else
+                nevList(end).FileTypes(end+1) = struct('Type',fileSplit{end},...
+                                'Location',[directoryLocation,filesep,matchingFiles(jj).name]);
+                end
+            end
+
+        end
+
+    
+end
+
+% do it for all subdirectories -- make this shiz recursive! 
+for ii = 1:length(subDirs)
+    nevList = find_Nev_Files(nevList,[directoryLocation,filesep,subDirs(ii).name]);
+end
 
 
 
